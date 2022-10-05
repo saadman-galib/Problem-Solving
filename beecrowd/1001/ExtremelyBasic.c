651a05bb9160bf861aebc9db127571d094074c43
saadman-galib <galib2829@gmail.com>
2022-10-05 21:18:04 +0600

    Modified ExtremelyBasic.c'

--- beecrowd/1001/ExtremelyBasic.c
+++ beecrowd/1001/ExtremelyBasic.c
@@ -2,10 +2,8 @@
 
 int main(){
     int A, B, X;
-    printf("Enter the value of A: ");
-    scanf("%d", &A);
-    printf("Enter the value of B: ");
-    scanf("%d", &B);
+    scanf("%d\n", &A);
+    scanf("%d\n", &B);
     X = A + B;
     printf("X = %d\n", X);
     return 0;

dd3e9334c435de609ef6b743bbf4c32d014bdaae
saadman-galib <galib2829@gmail.com>
2022-10-05 21:14:54 +0600

    move ExtremelyBasic.c to the right directory

---
+++ beecrowd/1001/ExtremelyBasic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int A, B, X;
+    printf("Enter the value of A: ");
+    scanf("%d", &A);
+    printf("Enter the value of B: ");
+    scanf("%d", &B);
+    X = A + B;
+    printf("X = %d\n", X);
+    return 0;
+}

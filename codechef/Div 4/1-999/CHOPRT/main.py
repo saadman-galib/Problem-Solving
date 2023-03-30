5c7dd28aee494c649fd64c512a43299ea55d0b32
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2023-03-30 06:21:43 +0600

    CHOPRT

---
+++ codechef/Div 4/1-999/CHOPRT/main.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, input().split())
+
+    if(a > b):
+        print(">")
+    elif(a < b):
+        print("<")
+    else:
+        print("=")

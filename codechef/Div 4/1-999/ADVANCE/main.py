a0a9315cbf758ce6ebce6ef011811208cd1bb904
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2023-04-01 00:55:48 +0600

    ADVANCE

--- codechef/Div 4/1-999/ADVANCE/main.py
+++ codechef/Div 4/1-999/ADVANCE/main.py
@@ -2,4 +2,9 @@ t = int(input())
 
 for i in range(t):
     x, y = map(int, input().split())
-    print("YES" if x + 200 <= y else "NO")
+    if (y < x):
+        print("NO")
+    elif(x + 200 < y):
+        print("NO")
+    else:
+        print("YES")

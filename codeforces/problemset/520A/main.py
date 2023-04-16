32933732180d3f9d5c7240dc990985e5b66f7aa1
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2023-04-17 00:12:46 +0600

    520A

---
+++ codeforces/problemset/520A/main.py
@@ -0,0 +1,45 @@
+n = int(input())
+s = input().lower()
+
+
+alphabet = {
+    'a': 0,
+    'b': 0,
+    'c': 0,
+    'd': 0,
+    'e': 0,
+    'f': 0,
+    'g': 0,
+    'h': 0,
+    'i': 0,
+    'j': 0,
+    'k': 0,
+    'l': 0,
+    'm': 0,
+    'n': 0,
+    'o': 0,
+    'p': 0,
+    'q': 0,
+    'r': 0,
+    's': 0,
+    't': 0,
+    'u': 0,
+    'v': 0,
+    'w': 0,
+    'x': 0,
+    'y': 0,
+    'z': 0
+}
+
+res = True
+
+
+for i in s:
+    alphabet[i] += 1
+
+for j in list(alphabet.values()):
+    if j == 0:
+        res = False
+        break
+
+print("YES" if res else "NO")

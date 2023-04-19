c787c49557dcf7e25908f8b7347093dd3f81d24a
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2023-04-20 05:02:22 +0600

    59A

--- codeforces/problemset/59A/main.py
+++ codeforces/problemset/59A/main.py
@@ -3,11 +3,8 @@ s = input()
 upper = 0
 lower = 0
 
-upper_letters = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-lower_letters = list("abcdefghijklmnopqrstuvwxyz")
-
 for i in s:
-    if i in upper_letters:
+    if i.isupper():
         upper += 1
     else:
         lower += 1

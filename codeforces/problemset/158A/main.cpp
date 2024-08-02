094ff2520c94f9c0d140cc80bf3a59eff5a12eba
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2024-08-03 04:22:26 +0600

    158A

---
+++ codeforces/problemset/158A/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Your code goes here
+    int n, k;
+    cin >> n >> k;
+
+    int arr[n];
+
+    int total = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i]; 
+    }
+
+    for (int i = 0; i < n; i++) {
+        total += (arr[i] >= arr[k - 1]) ? 1 : 0;
+    }
+
+    cout << total << endl;
+    return 0;
+}

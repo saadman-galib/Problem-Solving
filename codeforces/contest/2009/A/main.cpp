48483c16484fecdcb264a3e075301b09fb80ed37
Saadman Galib <73209315+saadman-galib@users.noreply.github.com>
2024-09-03 22:42:22 +0600

    2009 A

---
+++ codeforces/contest/2009/A/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h> // include all standard headers
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+
+const int INF = 1e9;
+const ll INFLL = 1e18;
+const int MOD = 1e9 + 7;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+   
+    int t; // number of test cases
+    cin >> t;
+    
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        
+        cout << b - a << endl;
+    }
+    
+    return 0;
+}
